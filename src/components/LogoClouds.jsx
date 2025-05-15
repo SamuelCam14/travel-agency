993adfcb4f1a3179fe8862c69e1a8e564a7960fe
Samuel Camargo <progreso.dev@proton.me>
2025-05-15 15:08:07 -0600

    Logo Clouds component

---
+++ src/components/LogoClouds.jsx
@@ -0,0 +1,53 @@
+export const LogoClouds = () => {
+  return (
+    <div className="bg-white py-12">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <h2 className="text-center text-lg/8 font-semibold text-gray-900">
+          Las mejores marcas confían en nuestra agencia de viajes para descubrir
+          el mundo
+        </h2>
+        <p className="mt-4 text-center text-base text-gray-600">
+          Expertos en experiencias únicas, destinos inolvidables y atención
+          personalizada. Viaja seguro con los líderes en turismo.
+        </p>
+        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+          <img
+            alt="Marriot International Logo"
+            src="marriot.jpeg"
+            width={158}
+            height={48}
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+          />
+          <img
+            alt="Riu Hotels Logo"
+            src="riu.png"
+            width={158}
+            height={48}
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+          />
+          <img
+            alt="Oasis Hotels Logo"
+            src="oasis.png"
+            width={158}
+            height={48}
+            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+          />
+          <img
+            alt="Fiesta Americana Logo"
+            src="fiestaame.png"
+            width={158}
+            height={48}
+            className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+          />
+          <img
+            alt="Palace Resorts Logo"
+            src="palace.jpeg"
+            width={158}
+            height={48}
+            className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/PopularDestination.jsx
+++ src/components/PopularDestination.jsx
@@ -1,5 +1,7 @@
 import { StarIcon } from "@heroicons/react/20/solid";
+import { div } from "framer-motion/client";
 import { useState, useRef, useEffect } from "react";
+import { LogoClouds } from "./LogoClouds";
 
 export const PopularDestination = () => {
   const [startIndex, setStartIndex] = useState(0);
@@ -17,42 +19,42 @@ export const PopularDestination = () => {
 
   const destinations = {
     1: {
-      image: "/Hero.webp",
+      image: "/turquia.jpg",
       title: "Turkey Vacation Package",
       description: "Visit Istanbul, Cappadocia & more with guided tours.",
       days: "7 Days / 6 Nights",
       rating: 4.7,
     },
     2: {
-      image: "/Hero.webp",
+      image: "/italia.webp",
       title: "Italy All-Inclusive Trip",
       description: "Enjoy Rome, Venice & Florence with curated experiences.",
       days: "7 Days / 6 Nights",
       rating: 4.9,
     },
     3: {
-      image: "/Hero.webp",
+      image: "/japon.jpg",
       title: "Japan Cultural Tour",
       description: "Explore Tokyo, Kyoto & temples in a guided adventure.",
       days: "7 Days / 6 Nights",
       rating: 4.8,
     },
     4: {
-      image: "/Hero.webp",
+      image: "/grecia.webp",
       title: "Greece Island Escape",
       description: "Sail the Aegean Sea and discover ancient Athens.",
       days: "8 Days / 7 Nights",
       rating: 4.6,
     },
     5: {
-      image: "/Hero.webp",
+      image: "/bali.webp",
       title: "Bali Retreat Package",
       description: "Relax in Ubud, beaches & rice terraces included.",
       days: "10 Days / 9 Nights",
       rating: 4.9,
     },
     6: {
-      image: "/Hero.webp",
+      image: "/egipto.jpg",
       title: "Egypt Explorer Trip",
       description: "Tour pyramids, the Sphinx & cruise the Nile River.",
       days: "9 Days / 8 Nights",
@@ -169,70 +171,76 @@ export const PopularDestination = () => {
   );
 
   return (
-    <section className="w-5/6 mx-auto mt-10 flex flex-col gap-5">
-      <div className="flex justify-between items-center">
-        <div className="flex flex-col gap-2">
-          <h2 className="header-text">Your Dream Vacation Awaits!</h2>
-          <p className="sub-text">
-            Discover top destinations and unforgettable experiences.
-          </p>
+    <div>
+      <LogoClouds />
+      <section className="w-5/6 mx-auto mt-10 flex flex-col gap-5">
+        <div className="flex justify-between items-center">
+          <div className="flex flex-col gap-2">
+            <h2 className="header-text">Your Dream Vacation Awaits!</h2>
+            <p className="sub-text">
+              Discover top destinations and unforgettable experiences.
+            </p>
+          </div>
+          <div className="flex gap-2">
+            <button
+              className="rounded-button bg-transparent text-gray-900 border-none font-stretch-ultra-condensed font-normal text-2xl"
+              onClick={handlePrev}
+            >
+              {"<"}
+            </button>
+            <button
+              className="rounded-button bg-gray-900 text-white border-gray-900 font-stretch-ultra-condensed font-normal text-2xl"
+              onClick={handleNext}
+            >
+              {">"}
+            </button>
+          </div>
         </div>
-        <div className="flex gap-2">
-          <button
-            className="rounded-button bg-transparent text-gray-900 border-none font-stretch-ultra-condensed font-normal text-2xl"
-            onClick={handlePrev}
+        <div>
+          <div
+            ref={sliderRef}
+            onTouchStart={handleTouchStart}
+            onTouchMove={handleTouchMove}
+            onTouchEnd={handleTouchEnd}
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-4"
           >
-            {"<"}
-          </button>
-          <button
-            className="rounded-button bg-gray-900 text-white border-gray-900 font-stretch-ultra-condensed font-normal text-2xl"
-            onClick={handleNext}
-          >
-            {">"}
-          </button>
-        </div>
-      </div>
-      <div>
-        <div
-          ref={sliderRef}
-          onTouchStart={handleTouchStart}
-          onTouchMove={handleTouchMove}
-          onTouchEnd={handleTouchEnd}
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-4"
-        >
-          {visibleDestinations.map(([key, destination]) => (
-            <div key={key} className="flex flex-col items-left gap-5">
-              <a
-                href="#"
-                className="hover:scale-105 transition-transform duration-300"
-              >
-                <div className="relative">
-                  <img
-                    src={destination.image}
-                    alt={destination.title}
-                    className="w-full h-60 object-cover rounded-4xl"
-                  />
-                  <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 bg-indigo-600 text-white rounded-4xl px-4 py-1 border-5 border-white">
-                    {destination.days}
+            {visibleDestinations.map(([key, destination]) => (
+              <div key={key} className="flex flex-col items-left gap-5">
+                <a
+                  href="#"
+                  className="hover:scale-105 transition-transform duration-300"
+                >
+                  <div className="relative">
+                    <img
+                      src={destination.image}
+                      alt={destination.title}
+                      className="w-full h-60 object-cover rounded-4xl"
+                    />
+                    <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2 bg-indigo-600 text-white rounded-4xl px-4 py-1 border-5 border-white">
+                      {destination.days}
+                    </div>
+                  </div>
+                </a>
+                <div className="flex justify-between items-end">
+                  <div>
+                    <h3 className="text-xl font-semibold mt-2">
+                      {destination.title}
+                    </h3>
+                    <p className="sub-text">{destination.description}</p>
+                  </div>
+                  <div className="flex items-center gap-1 mt-2 bg-indigo-600 w-fit h-fit py-2 px-4 text-center rounded-4xl">
+                    <StarIcon
+                      className="size-5 text-white"
+                      aria-hidden="true"
+                    />
+                    <p className="text-white">{destination.rating}</p>
                   </div>
                 </div>
-              </a>
-              <div className="flex justify-between items-end">
-                <div>
-                  <h3 className="text-xl font-semibold mt-2">
-                    {destination.title}
-                  </h3>
-                  <p className="sub-text">{destination.description}</p>
-                </div>
-                <div className="flex items-center gap-1 mt-2 bg-indigo-600 w-fit h-fit py-2 px-4 text-center rounded-4xl">
-                  <StarIcon className="size-5 text-white" aria-hidden="true" />
-                  <p className="text-white">{destination.rating}</p>
-                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
